3ed0e83ad10ff4bb90d98b3759073bd39305d417
unknown <tay721>
2023-06-07 21:06:24 +1000

    change port

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const cors = require("cors");
 const app = express();
-const PORT = 3003;
+const PORT = 3030;
 const routes = require("./route");
 
 app.use(cors());
